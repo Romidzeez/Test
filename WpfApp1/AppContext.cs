05f0b3af3b45d902a390a682ebd7dde85b95e7bf
Роман Киселев <Kiselevromidze@gmail.com>
2024-02-22 01:42:22 +0700

    работает

---
+++ WpfApp1/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WpfApp1
+{
+    internal class AppContext : DbContext
+    {
+        public DbSet<Classroom> Classrooms { get; set; }
+        public AppContext() : base("DefaultConnection") { }
+    }
+}
---
+++ WpfApp1/Rasspisanie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Classroom
+    {
+        private int Id_Classroom { get; set; }
+        private int Year_Of_Education;
+        private string Class_Letter;
+
+        public Classroom() { 
+        
+        }
+
+        public Classroom(int Id_Classroom, int Year_OF_Education, string Class_Letter)
+        {
+            this.Id_Classroom = Id_Classroom;
+            this.Year_Of_Education = Year_OF_Education;
+            this.Class_Letter = Class_Letter;
+        }
+
+    }
+}
