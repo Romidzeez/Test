c9dd25c412bdcf30c9c285a6e0f6fa221eab1b5e
Роман Киселев <Kiselevromidze@gmail.com>
2024-02-22 18:17:58 +0700

    ##

---
+++ WpfApp1/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Teacher
+    {
+        private int Id_Teacher { set; get; }
+        private string Full_Name;
+        private int Date_Of_birt;
+
+        public Teacher() { }
+        
+        public Teacher(int Id_Teacher, string Full_Name, int Date_Of_birt)
+        {
+            this.Id_Teacher= Id_Teacher;
+            this.Full_Name= Full_Name;
+            this.Date_Of_birt= Date_Of_birt;
+        }
+
+    }
+}
