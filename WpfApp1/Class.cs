80759bbb10ca6c4e1585287238d85f410685a5c9
Роман Киселев <Kiselevromidze@gmail.com>
2024-02-25 23:53:33 +0700

    update classes

---
+++ WpfApp1/Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Class
+    {
+        private int Id_Class{ get; set; }
+        private int Year_Of_Education;
+        private string Class_letter;
+
+        public Class() { }
+
+        public Class(int id_Class, int Year_Of_Education, string Class_Letter)
+        {
+            this.Id_Class = id_Class;
+            this.Year_Of_Education = Year_Of_Education;
+            this.Class_letter = Class_Letter;
+        }
+    }
+}
---
+++ WpfApp1/Class_Time.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Class_Time
+    {
+
+        private int Id_Class_time { set; get; }
+
+        private int Num_In_Day;
+        private int Start_Time;
+        private int End_Time;
+        private int Lesson_Duration;
+
+    }
+}
